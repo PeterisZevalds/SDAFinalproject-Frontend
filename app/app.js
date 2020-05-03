ededabc7537e7541c421ecd3059ae26182220e16
PeterisZevalds <54576938+PeterisZevalds@users.noreply.github.com>
2020-05-03 16:31:45 +0300

    updated project

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
+  'myApp.home',
   'myApp.view2',
   'myApp.userRegister',
   'myApp.userLogin',
@@ -12,5 +12,5 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
